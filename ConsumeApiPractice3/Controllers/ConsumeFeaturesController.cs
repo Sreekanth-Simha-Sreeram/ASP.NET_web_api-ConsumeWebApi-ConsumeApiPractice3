eea47ab081a42d3ea02d32929771d696d0b5f21c
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 14:20:44 +0530

    Code Added

---
+++ ConsumeApiPractice3/Controllers/ConsumeFeaturesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeApiPractice3.Controllers
+{
+    public class ConsumeFeaturesController : Controller
+    {
+       //code here to initiate a delete request from a client.
+
+        //code here to use sorting and paging by your client.
+
+        //code here to Perform HTTP caching in MVC client and show how to consume HTTP caching.
+    }
+}
